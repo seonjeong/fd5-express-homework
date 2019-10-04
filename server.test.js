48eb3100c74653de0219c3e24223fc1cf6af9f7a
sj <theblessedcity@hanmail.net>
2019-10-05 02:50:20 +0900

    회원가입 페이지 테스트

--- server.test.js
+++ server.test.js
@@ -30,3 +30,24 @@ describe('login 페이지 테스트', () => {
         });
     });
 });
+
+describe('register 페이지 테스트', () => {
+
+    test('실패 - 이미 존재하는 회원임',(done)=>{
+        request(app).post('/register')
+            .send({email: 'suho.kim2@gmail.com',password:'1234',name:'김수호2'})
+            .then((response)=> {
+                expect(response.text).toBe('이미 존재하는 회원입니다');
+                done();
+            });
+    });
+
+    test('성공',(done)=>{
+        request(app).post('/register')
+            .send({email: 'suho.kim2@gmail.com1',password:'1234'})
+            .then((response)=> {
+                expect(response.text).toBe('Found. Redirecting to /login');
+                done();
+            });
+    });
+});
